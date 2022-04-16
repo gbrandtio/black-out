f180c41bad534b28519ab31f4ef1657557d1c0d7
Ioannis Brandt-Ioannidis <giannisbrant@gmail.com>
2022-04-16 21:11:01 +0300

    Segragation of Parser. Making it specific only for handling outage specific parsing and conversions. Adding docs for Rest service.

--- Client/lib/services/Rest.dart
+++ Client/lib/services/Rest.dart
@@ -2,37 +2,37 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
-///Base service that performs all the common REST requests.
+/// Base service that performs all the common REST requests.
 class Rest{
 
-  ///Performs a POST request.
-  ///url: The url to perform the request to.
-  ///headers: The headers of the request (must include the authentication key for all the requests).
-  ///body: The body of the request.
+  /// Performs a POST request.
+  /// url: The url to perform the request to.
+  /// headers: The headers of the request (must include the authentication key for all the requests).
+  /// body: The body of the request.
   static Future<Response> doPOST(String url, Map<String,String> headers, Map<String, String> body) async{
     return http.post(Uri.parse(url),
           headers: headers,
           body: jsonEncode(body));
   }
 
-  ///Performs a PUT request.
-  ///url: The url to perform the request to.
-  ///headers: The headers of the request (must include the authentication key for all the requests).
-  ///body: The body of the request.
+  /// Performs a PUT request.
+  /// url: The url to perform the request to.
+  /// headers: The headers of the request (must include the authentication key for all the requests).
+  /// body: The body of the request.
   static Future<Response> doPUT(String url, Map<String,String> headers, dynamic body) async{
       return http.put(Uri.parse(url), headers: headers, body: jsonEncode(body));
   }
 
-  ///Performs a GET request.
-  ///url: The url to perform the request to.
-  ///headers: The headers of the request (must include the authentication key for all the requests).
+  /// Performs a GET request.
+  /// url: The url to perform the request to.
+  /// headers: The headers of the request (must include the authentication key for all the requests).
   static Future<Response> doGET(String url, Map<String, String> headers) async{
       return http.get(Uri.parse(url), headers: headers);
   }
 
-  ///Performs a DELETE request.
-  ///url: The url to perform the request to.
-  ///headers: The headers of the request (must include the authentication key for all the requests).
+  /// Performs a DELETE request.
+  /// url: The url to perform the request to.
+  /// headers: The headers of the request (must include the authentication key for all the requests).
   static Future<Response> doDELETE(String url, Map<String, String> headers) async{
       return http.delete(Uri.parse(url), headers: headers);
   }
