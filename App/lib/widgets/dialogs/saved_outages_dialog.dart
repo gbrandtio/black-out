455dd474af9e3667fecd56585258dc8ac8a92a46
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-03-27 00:09:02 +0400

    Decorating dialog widgets

--- App/lib/widgets/dialogs/saved_outages_dialog.dart
+++ App/lib/widgets/dialogs/saved_outages_dialog.dart
@@ -2,6 +2,9 @@ import 'package:black_out_groutages/services/outage_retrieval_service.dart';
 import 'package:flutter/material.dart';
 import '../components/saved_outage_list_item.dart';
 
+/// ----------------------------------------------------------------------------
+/// saved_outages_dialog.dart
+/// ----------------------------------------------------------------------------
 /// Offers a common dialog popup for displaying the saved outages.
 class SavedOutagesDialog extends StatefulWidget {
   const SavedOutagesDialog({Key? key}) : super(key: key);
