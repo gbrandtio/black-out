0e481d25c81bf272e1e6f418e0cb3c6808578f9f
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-04-01 01:35:59 +0400

    Removing headers and bosy params - relying on PROXY API

--- client/lib/services/rest.dart
+++ client/lib/services/rest.dart
@@ -7,21 +7,6 @@ import 'package:http/http.dart';
 /// ----------------------------------------------------------------------------
 /// Base service that implements all the common REST requests.
 class Rest {
-  /// Mandatory Req Headers for the outages requests.
-  static const Map<String, String> outagesRequestHeaders = {
-    "Accept": "*/*",
-    "Accept-encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "Cookie": "incap_ses_774_2807703=vLFVByVel2yjvlb58c29CvMaJ2QAAAAAndtLPywWqWZglPcWwEwp6w==; visid_incap_2807703=J94NR4YuSWWhmQF09Mwo/aoaJ2QAAAAAQUIPAAAAAAB3Y9NSoRGOe1civSNxKyAT",
-    "Host": "black-out.vercel.app",
-    "User-Agent": "PostmanRuntime/7.31.3"
-  };
-
-  /// Mandatory Req Body for the outages request.
-  static const Map<String, String> outagesRequestBody = {
-    "X-Requested-With": "XMLHttpRequest"
-  };
-
   /// Performs a POST request.
   /// url: The url to perform the request to.
   /// headers: The headers of the request (must include the authentication key for all the requests).
