58c9aa459d141ac6859ba6c68c4b1c6393b7b134
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-03-31 21:46:59 +0400

    Trying to bypass error by imitating Postman headers

--- client/lib/services/rest.dart
+++ client/lib/services/rest.dart
@@ -9,24 +9,12 @@ import 'package:http/http.dart';
 class Rest {
   /// Mandatory Req Headers for the outages requests.
   static const Map<String, String> outagesRequestHeaders = {
-    ":authority:": "siteapps.deddie.gr",
-    ":method:": "GET",
-    ":scheme:": "https",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9,el;q=0.8",
-    "cache-control": "max-age=0",
-    "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "Windows",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Access-Control-Allow-Origin": "https://black-out.vercel.app/#/",
+    "Accept": "*/*",
+    "Accept-encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Cookie": "incap_ses_774_2807703=vLFVByVel2yjvlb58c29CvMaJ2QAAAAAndtLPywWqWZglPcWwEwp6w==; visid_incap_2807703=J94NR4YuSWWhmQF09Mwo/aoaJ2QAAAAAQUIPAAAAAAB3Y9NSoRGOe1civSNxKyAT",
+    "Host": "black-out.vercel.app"
+    "User-Agent": "PostmanRuntime/7.31.3"
   };
 
   /// Mandatory Req Body for the outages request.
