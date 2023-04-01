7dd5c364d3faa85696ab51ed5ea1a1a8b2fc4d74
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-04-01 15:47:04 +0400

    Handling notifications badge visibility & notifications count

--- client/lib/widgets/base.dart
+++ client/lib/widgets/base.dart
@@ -1,3 +1,7 @@
+import 'package:black_out_groutages/services/data_persist.dart';
+import 'package:black_out_groutages/widgets/components/badge_button.dart';
+
+import '../models/outage_dto.dart';
 import 'navigation/app_bar.dart';
 import 'navigation/bottom_bar.dart';
 import 'package:flutter/material.dart';
@@ -25,9 +29,30 @@ class _BaseState extends State<Base> {
     setState(() {
       if (selectedIndex < BottomBar.screens.length) {
         selectedIndex = index;
+        if (selectedIndex == 1) {
+          BadgeButton.notificationsSeen = true;
+          changeNotificationsBadgeVisibility(calculateBadgeButtonVisibility());
+        }
       } else {
         selectedIndex = 0;
       }
+    });
+  }
+
+  bool calculateBadgeButtonVisibility() {
+    List<OutageDto> outages = DataPersistService()
+        .getSavedOutages(DataPersistService.notificationsOutagesList);
+    outages = OutageDto.filterOutagesList(outages);
+
+    bool badgeButtonVisibility = outages.isNotEmpty;
+    BadgeButton.numberOfNotifications = outages.length;
+
+    return BadgeButton.notificationsSeen ? false : badgeButtonVisibility;
+  }
+
+  void changeNotificationsBadgeVisibility(bool newVisibility) {
+    setState(() {
+      BadgeButton.isVisible = newVisibility;
     });
   }
 
@@ -36,6 +61,7 @@ class _BaseState extends State<Base> {
   /// of the [screens].
   @override
   Widget build(BuildContext context) {
+    changeNotificationsBadgeVisibility(calculateBadgeButtonVisibility());
     return Scaffold(
         appBar: BaseAppBar(appBar: AppBar()),
         body: Container(child: BottomBar.screens[selectedIndex]),
