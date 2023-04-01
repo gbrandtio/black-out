658593228cad75057b1533f1c40500b5551e4499
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-04-01 15:47:42 +0400

    UI/UX of notification list items

---
+++ client/lib/widgets/components/notification_list_item.dart
@@ -0,0 +1,71 @@
+import 'package:black_out_groutages/services/data_persist.dart';
+import 'chip_widget.dart';
+import 'package:flutter/material.dart';
+import '../../models/outage_dto.dart';
+
+/// ----------------------------------------------------------------------------
+/// notification_list_item.dart
+/// ----------------------------------------------------------------------------
+/// Representation of a single notification item to be displayed as part of a list.
+class NotificationListItem extends StatelessWidget {
+  final OutageDto outage;
+
+  const NotificationListItem({Key? key, required this.outage})
+      : super(key: key);
+
+  /// Builds the card that shows all the relevant outage information.
+  Widget listItem(BuildContext context) {
+    double threshold = 0;
+    bool isScreenWide = MediaQuery.of(context).size.width >= threshold;
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 5,
+      shadowColor: Colors.black,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ListTile(
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              leading: Container(
+                padding: EdgeInsets.only(right: 12.0),
+                decoration: new BoxDecoration(
+                    border: new Border(
+                        right:
+                            new BorderSide(width: 1.0, color: Colors.white24))),
+                child: Icon(Icons.autorenew, color: Colors.black),
+              ),
+              title: Text(
+                "Διακοπή ρεύματος στον Νομό ${outage.prefecture} Δήμο ${outage.municipality}",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+              subtitle: Row(
+                children: <Widget>[
+                  Icon(Icons.linear_scale, color: Colors.black),
+                  ChipWidget(
+                      color: const Color.fromRGBO(230, 170, 5, 1),
+                      label: outage.fromDatetime + " - " + outage.toDatetime),
+                  ChipWidget(
+                      color: const Color(0xFFB00020), label: outage.reason)
+                ],
+              ),
+              trailing: Icon(Icons.keyboard_arrow_right,
+                  color: Colors.black, size: 30.0))
+        ],
+      ),
+    );
+  }
+
+  /// Builds the card widget that will be shown to the user with the outage specific data.
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: listItem(context));
+  }
+}
