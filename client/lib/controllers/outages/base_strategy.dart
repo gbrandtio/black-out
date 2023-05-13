1a5f39a9ca021e97e12767dea86d2abcc9257173
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-05-14 02:15:57 +0400

    Strategy pattern for Outages Retrieval
    Implementation of a standard strategy pattern in order to decide seamlessly whether to retrieve the outages from the official source (i.e., perform a request and get a fresh copy of the current outages) or retrieve the already retrieved & saved outages from persistent storage.

---
+++ client/lib/controllers/outages/base_strategy.dart
@@ -0,0 +1,18 @@
+import 'package:black_out_groutages/models/prefecture_dto.dart';
+import '../../widgets/components/outage_list_item.dart';
+
+abstract class BaseOutagesControllerStrategy {
+  List<OutageListItem> outagesList = List<OutageListItem>.empty(growable: true);
+  PrefectureDto defaultPrefecture = PrefectureDto.defaultPrefecture();
+
+  void update(PrefectureDto selectedPrefecture) {
+    reset();
+    updateOutagesList(selectedPrefecture);
+  }
+
+  void reset() {
+    outagesList.clear();
+  }
+
+  Future<List<OutageListItem>> updateOutagesList(PrefectureDto selectedPrefecture);
+}
